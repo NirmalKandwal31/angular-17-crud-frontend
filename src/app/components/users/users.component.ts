5a306f1439abe368274f02e1cbe6354dfe87f19e
Nirmal Kandwal <nirmal.kandwal31@gmail.com>
2024-06-20 14:34:36 +0100

    Added Filter

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,6 +8,8 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { catchError, of } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-users',
@@ -19,6 +21,8 @@ import { catchError, of } from 'rxjs';
     MatIconModule,
     RouterLink,
     MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
@@ -28,6 +32,7 @@ export class UsersComponent implements OnInit {
 
   // Using signals for state management
   users = signal<Users[]>([]);
+  filteredUsers = signal<Users[]>([]);
   errorMessage = signal<string | null>(null);
   displayedColumns = signal<string[]>([
     'name',
@@ -52,6 +57,7 @@ export class UsersComponent implements OnInit {
       .subscribe({
         next: (res) => {
           this.users.set(res);
+          this.filteredUsers.set(res);
         },
         error: (err) => {
           this.errorMessage.set(
@@ -82,6 +88,9 @@ export class UsersComponent implements OnInit {
         .subscribe({
           next: (res) => {
             this.users.set(this.users().filter((user) => user._id !== id));
+            this.filteredUsers.set(
+              this.filteredUsers().filter((user) => user._id !== id)
+            );
           },
           error: (err) => {
             this.errorMessage.set(
@@ -94,5 +103,16 @@ export class UsersComponent implements OnInit {
           },
         });
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+      .toLowerCase()
+      .trim();
+    this.filteredUsers.set(
+      this.users().filter((user) =>
+        user.name.toLowerCase().includes(filterValue)
+      )
+    );
   }
 }
